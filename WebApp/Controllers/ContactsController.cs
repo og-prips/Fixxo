0e53fcf746389f34f631eb67f1eeea3ec0ad6aae
Robin Andersson <robin.andersson@me.com>
2023-04-18 19:11:44 +0200

    Initial commit

---
+++ WebApp/Controllers/ContactsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _01_AspNetMVC.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
